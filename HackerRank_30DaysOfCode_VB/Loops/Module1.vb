edb507e492ae9dbaa79571f6a191bd58d92058ed
harrimuhammad <harrimuhammad@gmail.com>
2016-10-12 22:32:00 +0700

    Day 5: Loops

---
+++ HackerRank_30DaysOfCode_VB/Loops/Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim n As Integer
+        Dim index As Integer
+        n = Console.ReadLine()
+        If n >= 2 And n <= 20 Then
+            For index = 1 To 10
+                Console.WriteLine(n.ToString() + " x " + index.ToString() + " = " + (n * index).ToString())
+            Next
+            Console.ReadLine()
+        End If
+
+    End Sub
+
+End Module
