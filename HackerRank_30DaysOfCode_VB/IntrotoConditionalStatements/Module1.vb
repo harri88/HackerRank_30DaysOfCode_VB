08709ab39fa551a0270bd9da710bb79826229c28
harrimuhammad <harrimuhammad@gmail.com>
2016-10-12 22:01:30 +0700

    Day 3: Intro to Conditional Statements

---
+++ HackerRank_30DaysOfCode_VB/IntrotoConditionalStatements/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim n As Integer
+        n = Console.ReadLine()
+
+        If (n >= 1 And n <= 100) Then
+            If (n Mod 2 = 1) Then
+                Console.WriteLine("Weird")
+            Else
+                If n >= 2 And n <= 5 Then
+                    Console.WriteLine("Not Weird")
+                ElseIf n >= 6 And n <= 20 Then
+                    Console.WriteLine("Weird")
+                Else
+                    Console.WriteLine("Not Weird")
+                End If
+            End If
+        End If
+    End Sub
+
+End Module
