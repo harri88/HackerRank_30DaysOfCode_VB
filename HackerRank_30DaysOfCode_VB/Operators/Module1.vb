fff3612eb92c8ed6b9e60a7dd7f46d1b1971bd59
harrimuhammad <harrimuhammad@gmail.com>
2016-10-12 21:30:19 +0700

    Day 2: Operators

---
+++ HackerRank_30DaysOfCode_VB/Operators/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim mealCost As Double
+        Dim tipPercent As Integer
+        Dim taxPercent As Integer
+        Dim totalCost As Double
+        Dim tip As Integer
+        Dim tax As Integer
+
+
+        mealCost = Console.ReadLine()
+        tipPercent = Console.ReadLine()
+        taxPercent = Console.ReadLine()
+
+        tip = mealCost * (tipPercent / 100)
+        tax = mealCost * (taxPercent / 100)
+        totalCost = mealCost + tip + tax
+
+        Console.WriteLine("The total meal cost is " + Math.Floor(totalCost).ToString() + " dollars.")
+        Console.ReadKey()
+    End Sub
+End Module
