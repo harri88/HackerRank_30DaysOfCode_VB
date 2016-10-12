c4d634e9fe412d978447bc1b19934648d8fcd11f
harrimuhammad <harrimuhammad@gmail.com>
2016-10-12 20:55:53 +0700

    Day 1: Data Types

---
+++ HackerRank_30DaysOfCode_VB/DataTypes/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim i As Integer = 4
+        Dim d As Double = 4.0
+        Dim s As String = "HackerRank "
+
+
+        Dim ii As Integer
+        Dim dd As Double
+        Dim ss As String
+
+        ii = Console.ReadLine()
+        dd = Console.ReadLine()
+        ss = Console.ReadLine()
+
+        Console.WriteLine(i + ii)
+        Console.WriteLine((d + dd).ToString("N1"))
+        Console.WriteLine(s + ss)
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ HackerRank_30DaysOfCode_VB/DataTypes/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DataTypes")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("DataTypes")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("520feec9-1a58-42bc-b770-65a93c15866c")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
